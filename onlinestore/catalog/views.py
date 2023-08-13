15652d14a18ebbd3b6d28c1a35234e14f1f7b6c6
DimaSouthTree <DITsindrin@gmail.com>
2023-08-14 00:57:41 +0300

    adde homework_19_2

---
+++ onlinestore/catalog/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name} ({phone}): {message}')
+
+    return render(request, 'catalog/contacts.html')
+
+
+
+
+# render(request, 'catalog/home.html')
